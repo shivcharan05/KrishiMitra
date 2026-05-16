86f52ff0080c1737ea8b8990d71e85ee82f55f2e
charan chintale <shivcharanchintale45@gmail.com>
2026-05-16 16:06:25 +0530

    database setup

--- backend/src/routes/testRoutes.js
+++ backend/src/routes/testRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const User = require("../models/User");
+const User = require('../models/User');
 const verifyFirebaseToken = require("../middleware/authMiddleware");
 
 router.post("/create-user", verifyFirebaseToken, async (req, res) => {
