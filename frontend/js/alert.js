9f16765d803a961c60dbc82d19e93ea1e4ee5b01
Shriraj <shrirajpatil17@gmail.com>
2026-05-16 15:45:06 +0530

    Frontend Phase-6 and Phase-7 Done!!

---
+++ frontend/js/alert.js
@@ -0,0 +1,8 @@
+/* =========================================
+   KRISHIMITRA AI — ALERTS
+========================================= */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("AI Alerts System Active");
+});
